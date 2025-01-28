3bd6446d92e3757448f275b3d8937f5903495de9
Gil Guereque <gguereque326@gmail.com>
2025-01-27 21:57:07 -0800

    commenting program.cs app entry point

--- GameStore/Backend/src/GameStore.Api/Program.cs
+++ GameStore/Backend/src/GameStore.Api/Program.cs
@@ -1,6 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
+// these lines are all about configuring this build
 var app = builder.Build();
 
+// the request pipeline
 app.MapGet("/", () => "Hello World!");
-
+// in this case we say any requests that arrive, 
+// we will reply with Hello World!
 app.Run();
