44dd1537c9fd73705df6f51995ead88ce7074d0b
Gil Guereque <gguereque326@gmail.com>
2025-01-29 20:21:16 -0800

    created Game class and associated properties

--- GameStore/Backend/src/GameStore.Api/Models/Game.cs
+++ GameStore/Backend/src/GameStore.Api/Models/Game.cs
@@ -1,8 +1,17 @@
 using System;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 namespace GameStore.Api.Models;
 
 public class Game
 {
+    public Guid Id { get; set; }
 
+    public required string Name { get; set; } //? makes the string able to be null, otherwise you would assign it an empty string
+
+    public required string Genre { get; set; }
+
+    public decimal Price { get; set; }
+
+    public DateOnly Release { get; set; } // we only care about the specific date
 }
