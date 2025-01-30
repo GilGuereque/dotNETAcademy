bd96fe27397e4b30c93381b1a615af408e800969
Gil Guereque <gguereque326@gmail.com>
2025-01-29 20:29:53 -0800

    Create games list for internal use

--- GameStore/Backend/src/GameStore.Api/Program.cs
+++ GameStore/Backend/src/GameStore.Api/Program.cs
@@ -1,6 +1,33 @@
+using GameStore.Api.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 // these lines are all about configuring this build
 var app = builder.Build();
+
+List<Game> games =
+[
+    new Game {
+        Id = Guid.NewGuid(),
+        Name = "Street Fighter II",
+        Genre = "Fighting",
+        Price = 19.99M,
+        ReleaseDate = new DateOnly(1992, 7, 15)
+    },
+    new Game {
+        Id = Guid.NewGuid(),
+        Name = "Final Fantasy XIV",
+        Genre = "Roleplaying",
+        Price = 59.99M,
+        ReleaseDate = new DateOnly(2010, 9, 30)
+    },
+    new Game {
+        Id = Guid.NewGuid(),
+        Name = "World of Warcraft",
+        Genre = "Massive Multiplayer Online",
+        Price = 49.99M,
+        ReleaseDate = new DateOnly(2004, 11, 23)
+    }
+]
 
 // the request pipeline
 app.MapGet("/", () => "Hello World!");
