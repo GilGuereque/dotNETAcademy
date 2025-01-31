e0dd640c36283c1c24b33847e50c93c82152d54a
Gil Guereque <gguereque326@gmail.com>
2025-01-30 21:37:36 -0800

    Implemented PUT call & https request with server side validation

--- GameStore/Backend/src/GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore/Backend/src/GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+521c093ab451bb77686d9461e57f264d3de85c26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+189dbdbe1f71efb40149b83c2cd42f7ef0f424e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
