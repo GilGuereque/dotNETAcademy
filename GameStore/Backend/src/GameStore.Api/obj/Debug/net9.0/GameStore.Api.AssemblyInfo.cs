3c0979c3c71455b7226c4ea8a77ddf6311643d4c
GilGuereque <gguereque326@gmail.com>
2025-01-31 16:49:46 -0800

    Set up Genres class and implemented this into the Games class instead of using a hardcoded string

--- GameStore/Backend/src/GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore/Backend/src/GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c692aebc979757292fcc76128698c150f295373")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea6806228085aef25f10466df744421beb04977e")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
