d0a95505e818f904239e72c499f3c894ec5eb8d7
Gil Guereque <gguereque326@gmail.com>
2025-02-01 19:51:36 -0800

    Updated POST http requests to use CreateGameDto

--- GameStore/Backend/src/GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore/Backend/src/GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34791cc08b735eb0df690351cc4647a84b30dabf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da6e6aab90ea73f47cf28875c612309666911349")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
