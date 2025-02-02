08e4bbd4abfaee9766283d6d05a074d9afe8cae7
Gil Guereque <gguereque326@gmail.com>
2025-02-01 20:01:15 -0800

    Updated PUT http request to use UpdateGameDto and cleaned up the Game.cs class file by removing unneccessary data annotations now

--- GameStore/Backend/src/GameStore.Api/Models/Game.cs
+++ GameStore/Backend/src/GameStore.Api/Models/Game.cs
@@ -1,21 +1,13 @@
-using System;
-using System.ComponentModel.DataAnnotations;
-using Microsoft.Extensions.Diagnostics.HealthChecks;
-
 namespace GameStore.Api.Models;
 
 public class Game
 {
     public Guid Id { get; set; }
 
-    [Required]
-    [StringLength(50)]
     public required string Name { get; set; } //? makes the string able to be null, otherwise you would assign it an empty string
 
     public required Genre Genre { get; set; }
 
-    [Required]
-    [Range(1, 100)]
     public decimal Price { get; set; }
 
     public DateOnly ReleaseDate { get; set; } // we only care about the specific date
--- GameStore/Backend/src/GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore/Backend/src/GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da6e6aab90ea73f47cf28875c612309666911349")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e87f38c9de20d7dbec2d8b95f314f373bcc3906d")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
