879e1fca8412429faf3b1455389d52aec947c4d4
Gil Guereque <gguereque326@gmail.com>
2025-02-02 17:55:09 -0800

    Created Features folder and Games folder inside that for implementing vertical slice architecture based on features

--- GameStore/Backend/src/GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore/Backend/src/GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+891833d860f89701ab1fd3677b47b69fa6568d06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc4249e870d0bde39caa75c151ce936fa42f513e")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
