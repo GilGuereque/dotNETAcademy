280d1edd4e9e7ed88d1fb0a53d06baa4d9e55ebc
Gil Guereque <gguereque326@gmail.com>
2025-02-05 22:04:56 -0800

    Implemented Singleton & Transient services for GameDataLogger & GameStoreData respectively

--- GameStore/Backend/src/GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore/Backend/src/GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16d613a2cd1bff7ff52789d7e27ded8b4453b27c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab0df559e84aee4df9aa5a331b8dba32275b78b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
