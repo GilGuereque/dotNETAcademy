e699789339924ca7ac0d9e39c0533f49d3d33af1
Gil Guereque <gguereque326@gmail.com>
2025-02-06 19:50:21 -0800

    update build files

--- GameStore/Backend/src/GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore/Backend/src/GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab0df559e84aee4df9aa5a331b8dba32275b78b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ed82360495a49976be3540d61e57be23962ca08")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
