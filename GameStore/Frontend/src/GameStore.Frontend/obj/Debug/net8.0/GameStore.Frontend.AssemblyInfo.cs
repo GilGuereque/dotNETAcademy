72d4e578900b6d681ca072083089ab2ecffc02d2
Gil Guereque <gguereque326@gmail.com>
2025-02-15 17:06:10 -0800

    configured frontend ui and added a new game

--- GameStore/Frontend/src/GameStore.Frontend/obj/Debug/net8.0/GameStore.Frontend.AssemblyInfo.cs
+++ GameStore/Frontend/src/GameStore.Frontend/obj/Debug/net8.0/GameStore.Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+321827a31008f1ebf67fc37eebe805777da18268")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1839dccb093d6d7bd17cb77b46f94cce125f915")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
