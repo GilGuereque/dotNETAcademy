3818de4923a04afd1100b0962c1a0e351f0c54af
Gil Guereque <gguereque326@gmail.com>
2025-02-16 21:37:59 -0800

    testing basic search

--- GameStore/Backend/src/GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore/Backend/src/GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13298d87e8998bc1030f911ff8c19d63ffa040f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad1b11c65e855575397660538b9da47095b5b54d")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
