e4bcd050c25c11a027d77010786df4bbbb418ddb
Gil Guereque <gguereque326@gmail.com>
2025-02-16 21:28:51 -0800

    debugging pagination

--- GameStore/Backend/src/GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore/Backend/src/GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c6fcb0f4f1a29031d2d21278bc552ad82f2218a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13298d87e8998bc1030f911ff8c19d63ffa040f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
