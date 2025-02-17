49a34925b8df38bbf7980696e472bb65b15a68c8
GilGuereque <gguereque326@gmail.com>
2025-02-17 15:35:40 -0800

    testing frontend for the swagger ui implementation

--- GameStore/Frontend/src/GameStore.Frontend/obj/Debug/net8.0/GameStore.Frontend.AssemblyInfo.cs
+++ GameStore/Frontend/src/GameStore.Frontend/obj/Debug/net8.0/GameStore.Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e847149838b618c00aad0e49df4a83c37010027a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4c560260187ad24f68b4b3355455270c1e5704c")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
