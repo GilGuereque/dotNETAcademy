ad1b11c65e855575397660538b9da47095b5b54d
Gil Guereque <gguereque326@gmail.com>
2025-02-16 21:35:34 -0800

    Implemented basic search on game names

--- GameStore/Backend/src/GameStore.Api/Features/Games/GetGames/GetGamesEndpoint.cs
+++ GameStore/Backend/src/GameStore.Api/Features/Games/GetGames/GetGamesEndpoint.cs
@@ -16,7 +16,12 @@ public static class GetGamesEndpoint
             // skipcount = (1 - 1) * 5 = 0 (skip 0 records and get first page)
             // skipcount = (2 - 1) * 5 = 5 (skip 5 records and get second page)
 
-            var gamesOnPage = await dbContext.Games
+            var filteredGames = dbContext.Games
+                                .Where(game => string.IsNullOrWhiteSpace(request.Name)
+                                        || game.Name.Contains(request.Name)); // Filter games by name (if provided)
+
+
+            var gamesOnPage = await filteredGames // Get the games on the current page (page number and page size)
                                 .OrderBy(game => game.Name) // Order by name (ascending)
                                 .Skip(skipcount) // Skip the first n records (n = skipcount) and get the next page of records
                                 .Take(request.PageSize) // Take the next n records (n = request.PageSize) and get the next page of records
@@ -31,7 +36,7 @@ public static class GetGamesEndpoint
                                 .AsNoTracking()
                                 .ToListAsync();
 
-            var totalGames = await dbContext.Games.CountAsync(); // Get the total number of games in the database
+            var totalGames = await filteredGames.CountAsync(); // Get the total number of games in the database
             var totalPages = (int)Math.Ceiling(totalGames / (double)request.PageSize); // Get the total number of pages (totalGames / PageSize)
             // 23 / 10 = 2.3 -> 3 pages (Math.Ceiling rounds up to the nearest whole number)
             return new GamesPageDto(totalPages, gamesOnPage); // Return the total number of pages and the games on the current page
