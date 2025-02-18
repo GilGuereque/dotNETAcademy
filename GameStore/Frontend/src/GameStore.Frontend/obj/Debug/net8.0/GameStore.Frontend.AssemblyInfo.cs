1a01c5ba63eaa2e9477ec6ac7911dcd2972ea34f
Gil Guereque <gguereque326@gmail.com>
2025-02-17 20:56:43 -0800

    Testing out GameStore website

--- GameStore/Frontend/src/GameStore.Frontend/obj/Debug/net8.0/GameStore.Frontend.AssemblyInfo.cs
+++ GameStore/Frontend/src/GameStore.Frontend/obj/Debug/net8.0/GameStore.Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9ed728b55daf3fb197ecbfa5f0df60780c31509")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a170b606c52e2d09b9794144ed94e74901e9d958")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
